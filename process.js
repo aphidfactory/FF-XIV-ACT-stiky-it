8dcaf37bc16497f8876b00bcff4f90e1085fb800
Xinkle(Sunook Choi) <cswcsy@gmail.com>
2019-12-31 16:38:10 +0900

    칠흑 패치에 따른 업데이트

--- process.js
+++ process.js
@@ -1,51 +1,35 @@
-
-try { document.addEventListener('onLogLine', onLogLine); } catch (ex) { console.log("Core Error : onLogLine is not defined."); }
-
-
-function onLogLine(data) {
-	var obj = data.detail;
-	var obj_pld = obj.payload;
-	var msgName;
-	var msgTxt;
-	var msgDetail;
-
-	//for(var key in obj){
-	//	console.log(key + "::" + obj[key]);
-	//}
-	//console.log("-----");
-
-	// 존잉
-	if(obj.opcode == "1"){	//01은 위치 타입(아마)
-		var objMM_key = obj_pld[1]; 
-		viewMM(objMM_key);
-	}
-
-	var strpos = null;
-	// 던전 진입 시 공략파일 확인
-	if(obj.opcode == "0" && obj_pld[0]== "0039"){	//00은 아나운스 타입, 0039는 시스템 타입(아마)
-		viewDG(obj_pld);
-	}
-
-	// 혼잣말로 키워드 확인할 때
-	if(obj.opcode == "0" && obj_pld[0]== "0038"){	//00은 아나운스 타입, 0038은 혼잣말
-		var retChk = viewDG(obj_pld);
-		if(retChk != "chk"){
-			var objMM_key = obj_pld[2]; 
-			viewMM(objMM_key);
-		}
-	}
-
+try {
+	console.log("Register event listener")
+	addOverlayListener('LogLine', onLogLine);
+	startOverlayEvents()
+} catch (ex) {
+	console.log("Core Error : onLogLine is not defined.");
 }
 
-function viewMM(objMM_key){
+function onLogLine(data) {
+	console.log(data)
+	var line = data['line']
+	var type = line[0]
+	var msgType = line[2]
+	var location = line[3]
 
+	if (type === "01" && msgType === "153") {
+		viewMM(location);
+	}
+
+	if (type === "01" && msgType === "9d") {
+		viewDG(location)
+	}
+}
+
+function viewMM(objMM_key) {
 	// 현재 위치를 표시 해 주고 화면 초기화.
-	$('#footer').html( objMM_key );
-	$('#content').html( '' );
+	$('#footer').html(objMM_key);
+	$('#content').html('');
 
 	// 던전 외 공략파일 확인
 	//공략이 없는 던전이다.
-	if(typeof objMM[objMM_key] === 'undefined'){
+	if (typeof objMM[objMM_key] === 'undefined') {
 		objMM_key_name = objMM_key;
 		objMM_key = "Default";
 	}
@@ -54,49 +38,34 @@ function viewMM(objMM_key){
 	msgTxt = objMM[objMM_key].txt;
 	msgDetail = "";
 
-	if(msgName === 'undefined' || msgName === null || msgName === "" || msgName === "Default"){
+	if (msgName === 'undefined' || msgName === null || msgName === "" || msgName === "Default") {
 		msgName = objMM_key_name;
 	}
 
 	msgDetail = "<span class='title'>※ " + msgName + "</span><p class='detail'>" + msgTxt + "</p>";
-	$('#content').html( msgDetail );
+	$('#content').html(msgDetail);
 }
 
-function viewDG(obj_pld){
-	var txt_dg = obj_pld[2]; 
-	var objDG_key = null;
-
-	if(obj_pld[0]== "0039"){
-		strpos = txt_dg.indexOf(" 공략을 시작합니다.");
+function viewDG(objDG_key) {
+	//공략이 없는 던전이다.
+	if (typeof objDG[objDG_key] === 'undefined') {
+		objDG_key_name = objDG_key;
+		objDG_key = "Default";
+		retChk = null;
+	} else {
+		retChk = "chk";
 	}
-	else if (obj_pld[0]== "0038"){
-		strpos = txt_dg.length;
+
+	msgName = objDG_key;
+	msgTxt = objDG[objDG_key].txt;
+	msgDetail = "";
+
+	if (msgName === 'undefined' || msgName === null || msgName === "" || msgName === "Default") {
+		msgName = objDG_key_name;
 	}
 
-	if(strpos != -1){	///던전이나 토벌전에 진입했다.
-		objDG_key = txt_dg.substring(0,strpos);
+	msgDetail = "<span class='title'>※ " + msgName + "</span><p class='detail'>" + msgTxt + "</p>";
+	$('#content').html(msgDetail);
 
-		//공략이 없는 던전이다.
-		if(typeof objDG[objDG_key] === 'undefined'){
-			objDG_key_name = objDG_key;
-			objDG_key = "Default";
-			retChk = null;
-		}
-		else{
-			retChk = "chk";
-		}
-
-		msgName = objDG_key;
-		msgTxt = objDG[objDG_key].txt;
-		msgDetail = "";
-
-		if(msgName === 'undefined' || msgName === null || msgName === "" || msgName === "Default"){
-			msgName = objDG_key_name;
-		}
-
-		msgDetail = "<span class='title'>※ " + msgName + "</span><p class='detail'>" + msgTxt + "</p>";
-		$('#content').html( msgDetail );
-
-		return retChk;
-	}
+	return retChk;
 }
