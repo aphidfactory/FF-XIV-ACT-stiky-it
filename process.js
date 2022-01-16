fe3a4bbf9e90be550580d1361ebb38b0095b44d0
aphidfactory <42764764+aphidfactory@users.noreply.github.com>
2022-01-16 13:41:55 +0900

    v2.0
    ACT 3.6.0 대응

--- process.js
+++ process.js
@@ -4,37 +4,34 @@ try { document.addEventListener('onLogLine', onLogLine); } catch (ex) { console.
 
 function onLogLine(data) {
 	var obj = data.detail;
-	var obj_pld = obj.payload;
+	var obj_pld = eval( obj );
 	var msgName;
 	var msgTxt;
 	var msgDetail;
 
-	//for(var key in obj){
-	//	console.log(key + "::" + obj[key]);
+	
+	//console.log(typeof obj_pld);
+	//for(var key in obj_pld){
+	//	console.log(key + "::" + obj_pld[key]);
 	//}
 	//console.log("-----");
 
 	// 존잉
-	if(obj.opcode == "1"){	//01은 위치 타입(아마)
-		var objMM_key = obj_pld[1]; 
-		viewMM(objMM_key);
-	}
-
-	var strpos = null;
-	// 던전 진입 시 공략파일 확인
-	if(obj.opcode == "0" && obj_pld[0]== "0039"){	//00은 아나운스 타입, 0039는 시스템 타입(아마)
+	if(obj_pld[0] == "01"){	//01은 위치 타입(아마)
+		var objMM_key = obj_pld[3]; 
+		//viewMM(objMM_key);
 		viewDG(obj_pld);
 	}
 
+
 	// 혼잣말로 키워드 확인할 때
-	if(obj.opcode == "0" && obj_pld[0]== "0038"){	//00은 아나운스 타입, 0038은 혼잣말
+	if(obj_pld[0] == "00" && obj_pld[2]== "0038"){	//00은 아나운스 타입, 0038은 혼잣말
 		var retChk = viewDG(obj_pld);
 		if(retChk != "chk"){
-			var objMM_key = obj_pld[2]; 
+			var objMM_key = obj_pld[4]; 
 			viewMM(objMM_key);
 		}
 	}
-
 }
 
 function viewMM(objMM_key){
@@ -63,18 +60,26 @@ function viewMM(objMM_key){
 }
 
 function viewDG(obj_pld){
-	var txt_dg = obj_pld[2]; 
+	var txt_dg = obj_pld[4]; 
 	var objDG_key = null;
+	var strpos = null;
 
-	if(obj_pld[0]== "0039"){
+	if(obj_pld[2]== "0839"){	//0839는 시스템 타입(아마)
 		strpos = txt_dg.indexOf(" 공략을 시작합니다.");
 	}
-	else if (obj_pld[0]== "0038"){
+	else if (obj_pld[2]== "0038"){	//0038은 혼잣말
 		strpos = txt_dg.length;
 	}
 
-	if(strpos != -1){	///던전이나 토벌전에 진입했다.
+
+	if(strpos != -1 && strpos != null){	///혼잣말로 불러왔거나, 혹은 던전이나 토벌전에 진입했다.
 		objDG_key = txt_dg.substring(0,strpos);
+	}
+	else{
+		objDG_key = obj_pld[3];
+	}
+
+	if(objDG_key != null){	
 
 		//공략이 없는 던전이다.
 		if(typeof objDG[objDG_key] === 'undefined'){
